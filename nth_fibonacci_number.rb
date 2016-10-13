9fa502b3d0a2d36d3ec1845aea8e3749bdeea8cf
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-10-13 13:31:46 -0600

    add fibonacci sequence problem with recursive solution

---
+++ nth_fibonacci_number.rb
@@ -0,0 +1,37 @@
+#### Instructions ####
+# Write a function fib() that a takes an integer nn and returns the nnth fibonacci number.
+# Let's say our fibonacci series is 0-indexed and starts with 0.
+# eg:
+# fib(0) => 0
+# fib(1) => 1
+# fib(2) => 1
+# fib(3) => 2
+# fib(4) => 3
+#-----------------------------------------------------------------#
+
+#Fibonacci sequence:
+#     a sequence in which each number is the sum of the two proceeding numbers
+nums    = [0, 1, 1, 2, 3, 5, 8, 13]
+indexes = [0, 1, 2, 3, 4, 5, 6, 7]
+#------------------------------------------------------------------------
+
+# Thoughts for solving
+#1) We know the sequence (in order to check our work), 
+  # but we don't have access to the sequence within the method
+#2) We know the starting point of the sequence, possible to use this to keep track:
+starting_num = 0
+starting_index = 0
+
+#-------------------------------------------------------------------------
+
+#Recursive solution:
+def fib(index)
+  if index == 0 || index == 1
+    return index
+  end
+  return fib(index - 1 ) + fib(index - 2)
+end
+
+#-------------------------------------------------------------------------
+
+#Iterative solution:
